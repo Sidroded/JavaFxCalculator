1cbde1bf42386c9db681580b2fd194d4639dc308
Sidroded <deinekindaniil21@gmail.com>
2022-11-18 14:11:30 +0100

    Start Calculator

---
+++ src/main/java/com/javafxcalculator/Model.java
@@ -0,0 +1,8 @@
+package com.javafxcalculator;
+
+public class Model {
+    public long calculation(long a, long b, String operator) {
+        return 0;
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javafxcalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.javafxcalculator to javafx.fxml;
+    exports com.javafxcalculator;
+}
